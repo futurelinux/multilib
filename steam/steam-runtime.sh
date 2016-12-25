085aac8a9eb20d4b20f4ccd3e9b06d7f99cc7c65
Samir Benmendil <me@rmz.io>
2016-12-25 23:55:49 +0000

    steam: make PKGBUILD more like arch for maintainability

--- steam/steam-runtime.sh
+++ steam/steam-runtime.sh
@@ -2,5 +2,4 @@
 
 # see https://chakraos.org/wiki/index.php?title=Steam#Missing_Direct_Rendering
 export LD_PRELOAD="/usr/lib/libstdc++.so.6 /usr/lib32/libstdc++.so.6 /usr/lib/libgcc_s.so.1 /usr/lib32/libgcc_s.so.1"
-
-/usr/bin/steam %U
+exec /usr/lib/steam/steam "$@"
