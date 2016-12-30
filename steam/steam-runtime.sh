a56f506e113918f005364c958155fbc777dee608
Samir Benmendil <me@rmz.io>
2016-12-30 00:06:46 +0000

    steam: optionally display kdialog warning

--- steam/steam-runtime.sh
+++ steam/steam-runtime.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+if hash kdialog 2>/dev/null; then
+    kdialog --dontagain steam_warnings_chakra:nofilemsg --msgbox \
+"Chakra Linux is not officially supported by Valve.
+For help visit the wiki page of Steam https://chakraos.org/wiki/index.php?title=Steam
+or ask for help in the forum"
+fi
+
 # see https://chakraos.org/wiki/index.php?title=Steam#Missing_Direct_Rendering
 export LD_PRELOAD='/usr/$LIB/libstdc++.so.6 /usr/$LIB/libgcc_s.so.1 /usr/$LIB/libxcb.so.1 /usr/$LIB/libgpg-error.so'
 exec /usr/lib/steam/steam "$@"
