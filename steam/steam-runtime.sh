551192e76e2e0e1ad684f799e7146bd0ef404b0a
Jeff Huang <s8321414@chakralinux.org>
2019-10-15 02:08:38 +0200

    [skip-ci] steam: fix lib32#6 and desktop#169

--- steam/steam-runtime.sh
+++ steam/steam-runtime.sh
@@ -3,10 +3,8 @@
 if hash kdialog 2>/dev/null; then
     kdialog --dontagain steam_warnings_chakra:nofilemsg --msgbox \
 "Chakra Linux is not officially supported by Valve.
-For help visit the wiki page of Steam https://chakraos.org/wiki/index.php?title=Steam
-or ask for help in the forum"
+For help, please visit https://community.chakralinux.org."
 fi
 
-# see https://chakraos.org/wiki/index.php?title=Steam#Missing_Direct_Rendering
 export LD_PRELOAD='/usr/$LIB/libstdc++.so.6 /usr/$LIB/libgcc_s.so.1 /usr/$LIB/libxcb.so.1 /usr/$LIB/libgpg-error.so'
 exec /usr/lib/steam/steam "$@"
